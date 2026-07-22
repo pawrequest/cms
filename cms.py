7eb34540eb0dcf8ccb9b69345626220819dc4dad
paw <pawrequest@users.noreply.github.com>
2026-07-22 21:19:52 +0100

    .

--- cms.py
+++ cms.py
@@ -1,19 +1,22 @@
 import os
 import subprocess
+import time
+import sys
 
 # Constants
 RTSP_USER = os.getenv("RTSP_USER")
 RTSP_PASS = os.getenv("RTSP_PASS")
 PROG_EXE = r"C:\Program Files (x86)\VideoLAN\VLC\vlc.exe"
 DEFAULT_CHANNELS = [2, 6]
-ETH_SCRIPT = r"E:\DOCS\Desktop\cms\gen_eth.bat"
 
-OFFICE_ETH = ("192.168.1.127", "192.168.1.254")
-HOME_ETH = ("192.168.0.127", "192.168.0.254")
+
+# OFFICE_ETH = ("192.168.1.127", "192.168.1.254")
+# HOME_ETH = ("192.168.0.127", "192.168.0.254")
 
 
 def change_ip(ip_address, gateway):
-    """Run the Ethernet configuration script."""
+    """Run the Ethernet configuration script to change subnets."""
+    # ETH_SCRIPT = r"E:\DOCS\Desktop\cms\bats\eth.bat"
     subprocess.run(
         [
             "powershell",
@@ -38,24 +41,29 @@ def close_vlc():
 
 
 def main():
-    change_ip(*OFFICE_ETH)
-
-    # Get channels to launch
-    user_channels = input("Enter channels (space-separated, default: 2 6): ").strip()
-    channels = list(map(int, user_channels.split())) if user_channels else DEFAULT_CHANNELS
+    # change_ip(*OFFICE_ETH)
+    channels = [2,6]
     launch_vlc_channels(channels)
+    # time.sleep(2)
+    # Get channels to launch
+    # user_channels = input("Enter channels (space-separated, default: 2 6): ").strip()
+    
+    
 
     while True:
         # User prompt
-        user_input = input("Press 'r' to refresh VLC windows, or 'q' to reset eth and quit: ").strip().lower()
+        user_input = input("Enter channels (space-separated) to change, 'r' to refresh VLC windows, or [ENTER] to quit:/n ").strip().lower()
         if user_input == "r":
             close_vlc()
             print("Refreshing VLC windows...")
             launch_vlc_channels(channels)
+        elif user_input.isdigit():
+            channels = list(map(int, user_channels.split())) if user_channels else channels
         else:
             close_vlc()
-            print("Resetting Ethernet...")
-            change_ip(*HOME_ETH)
+            # print("Resetting Ethernet...")
+            # change_ip(*HOME_ETH)
+            sys.exit(0)
 
 
 if __name__ == "__main__":
--- pwsh/cms.ps1
+++ pwsh/cms.ps1
@@ -1,14 +1,16 @@
 $script:VIDEO_PLAYER = "C:\Program Files (x86)\VideoLAN\VLC\vlc.exe"
 $script:RTSP_USER = $env:RTSP_USER
 $script:RTSP_PASS = $env:RTSP_PASS
+$script:RTSP_IP = $env:RTSP_IP
 $script:FRONT_CHANNELS = @(1, 2, 6, 8)
 $script:FRONT_MAIN = @(1)
 $script:DOOR_CHANNELS = @(2, 6)
 $script:OFF_CHANNELS = @(4, 10)
+$script:ALL_CHANNELS = @(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
 
 function Open-Chanel([int]$Channel, [int]$Stream = 1, [string]$Codec = "H264") {
     Write-Host "Launching channel $Channel..."
-    $baseUrl = "rtsp://${RTSP_USER}:${RTSP_PASS}@192.168.1.10:554"
+    $baseUrl = "rtsp://${RTSP_USER}:${RTSP_PASS}@192.168.1.${RTSP_IP}:554"
     $url = $baseUrl + "?codec=$Codec&channel=$Channel&stream=$Stream.sdp&real_stream--rtp-caching=100"
     Start-Process -FilePath $script:VIDEO_PLAYER -ArgumentList $url
 }
@@ -16,12 +18,12 @@ function Open-Chanel([int]$Channel, [int]$Stream = 1, [string]$Codec = "H264") {
 
 function UILoop([array]$channels = $null) {
     $streamQuality = 1 # 0: high quality, 1: low quality
-    if (-not $channels) { $channels = $script:FRONT_MAIN }
+    if (-not $channels) { $channels = $script:FRONT_CHANNELS }
     while ($true) {
         foreach ($chan in $channels)
         { Open-Chanel -Channel $chan -Stream $script:STREAM_QUALITY }
         $inputted = Read-Host "Choose option:
-        `n`t[d]oors [f]ront [o]ffice (change channels)
+        `n`t[d]oors [f]ront [o]ffice [a]ll (change channels)
         `n`t[r]eload viewers
         `n`t[u]pgrade quality, [y]downgrade quality,
         `n`t[Enter] tidy up and close`n"
@@ -49,6 +51,10 @@ function UILoop([array]$channels = $null) {
             Write-Host "Switching to office channels..."
             $channels = $OFF_CHANNELS
         }
+        elseif ($inputted -ieq "a") {
+            Write-Host "Switching to all channels..."
+            $channels = $ALL_CHANNELS
+        }
         else {
             Write-Host "Closing UI..."
             break
@@ -57,5 +63,6 @@ function UILoop([array]$channels = $null) {
 }
 
 
-If ((Resolve-Path -Path $MyInvocation.InvocationName).ProviderPath -eq $MyInvocation.MyCommand.Path)
-{ UILoop -Channels $args }
+If ((Resolve-Path -Path $MyInvocation.InvocationName).ProviderPath -eq $MyInvocation.MyCommand.Path) {
+    UILoop -Channels $args
+}
